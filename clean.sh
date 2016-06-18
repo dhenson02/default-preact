e57a7cd7b20f2fbc59671d176df55a9c0cb5f913
Deryck Henson <dhenson@viztek.net>
2016-06-18 12:57:30 -0400

    ...

--- clean.sh
+++ clean.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 if [ -d ".tmp/" ];then
-    rm -f ".tmp/*.html"
     if [ -d ".tmp/css" ];then
-        rm -rf ".tmp/css/*"
+        rm -rf .tmp/css/*
     else
-        mkdir ".tmp/css"
+        mkdir .tmp/css
     fi
     if [ -d ".tmp/js" ];then
-        rm -rf ".tmp/js/*"
+        rm -rf .tmp/js/*
     else
-        mkdir ".tmp/js"
+        mkdir .tmp/js
     fi
 else
-    mkdir -p ".tmp/{css,js}"
+    mkdir -p .tmp/css .tmp/js
 fi
-
